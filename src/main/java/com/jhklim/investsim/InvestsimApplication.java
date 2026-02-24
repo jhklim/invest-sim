5c877aa008004f688ab683fc05f4e06e4cc71937
jaehyucklim <jhlim087@gmail.com>
2026-02-24 15:23:25 +0900

    chore: initialize project structure

---
+++ src/main/java/com/jhklim/investsim/InvestsimApplication.java
@@ -0,0 +1,13 @@
+package com.jhklim.investsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestsimApplication.class, args);
+	}
+
+}
